e1df2726a232e547fad445e7198019bbfcbbe31b
RoamingCookie <82594422+RoamingCookie@users.noreply.github.com>
2024-03-11 13:40:36 +0530

    fix

--- cookielist/__init__.py
+++ cookielist/__init__.py
@@ -6,8 +6,6 @@ from rich import pretty, traceback
 
 from cookielist.environment import env
 
-from rich import print
-print(dict(os.environ))
 env.path("COOKIELIST_STATE_FOLDER").mkdir(parents=True, exist_ok=True)
 
 if os.getenv("COOKIELIST_DEBUG").lower() == "true":
